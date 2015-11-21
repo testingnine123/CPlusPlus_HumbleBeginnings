54d65281195e5e84f188a3bb56b9acb31710350e
Testingnine123 <testingnine123@gmail.com>
2015-11-21 16:57:08 -0500

    Important example to show how to create a separate header file and reference that in the main cpp file.

---
+++ Class_Example_HeaderFiles/constructors2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include "printName.h"	
+#include<string>
+using namespace std;
+
+
+printName::printName(string nm) //constructors can have parameters as well
+{
+	setName(nm);
+}
+
+void printName::setName(string x)
+{
+	name = x;
+}
+
+string printName::getName()
+{
+	return name;
+}
+
+int main()
+{
+	string name;
+	cout<<"Enter a name:";
+	
+	cin>>name;
+	
+	printName obj1(name); //the entered string will be stored in "name" private variable
+	
+	cout<<"Name entered was:"<<obj1.getName()<<"\n";
+	
+	return 0;
+}
---
+++ Class_Example_HeaderFiles/printName.h
@@ -0,0 +1,11 @@
+//#include<string>
+class printName
+{
+	private:
+		std::string name;
+	
+	public:
+		printName(std::string nm);
+		void setName(std::string x);		
+		std::string getName();
+};
