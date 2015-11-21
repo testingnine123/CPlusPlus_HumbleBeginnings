e9b2b700928433dc3c26ce52630971240b50ac2e
Testingnine123 <testingnine123@gmail.com>
2015-11-21 16:32:57 -0500

    Basic usage of constructors in C++.

---
+++ constructors.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class myClass
+{
+	private:
+		string name;
+	
+	public:
+		myClass(string nm) //constructors can have parameters as well
+		{
+			 setName(nm);
+		}
+		void setName(string x)
+		{
+			name = x;
+		}
+		string getName()
+		{
+			return name;
+		}
+};
+int main()
+{
+	string name;
+	cout<<"Enter a name:";
+	
+	cin>>name;
+	
+	myClass obj1(name); //the entered string will be stored in "name" private variable
+	
+	cout<<"Name entered was:"<<obj1.getName()<<"\n";
+	
+	return 0;
+}
