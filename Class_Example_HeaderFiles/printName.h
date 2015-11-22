19d56f7ef95c550c458ef05fd1da4a8efc80f990
Testingnine123 <testingnine123@gmail.com>
2015-11-22 01:08:18 -0500

    Important example to show how to create a separate header file and reference that in the main cpp file.

--- Class_Example_HeaderFiles/printName.h
+++ Class_Example_HeaderFiles/printName.h
@@ -1,4 +1,5 @@
-#ifndef printName_h
+#ifndef printName_h //ifndef stands for "if not defined". The first pair of statements tells the program to define the MyClass header file if it has not been defined already. This prevents a header file from being included more than once within one file.
+endif ends the condition.
 #define printName_h
 
 class printName
@@ -13,4 +14,4 @@ class printName
 		std::string getName();
 };
 
-#endif /* Header_h */
+#endif /* Header_h */ //endif ends the condition.
